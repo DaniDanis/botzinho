691a9c950f5665a968da7cfb474894cb3d339347
DaniDanis <daniel.dantas.98@gmail.com>
2023-01-27 01:25:59 -0300

    Melhorado o web scrap, agora o df vem com informações melhores

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 from time import sleep
 from selenium import webdriver
@@ -15,5 +14,12 @@ tabela = driver.find_element(By.XPATH, value='//*[@id="KillStatisticsTable"]')
 tabela_html = tabela.get_attribute('outerHTML')
 df = pd.read_html(str(tabela_html))
 df = df[0]
+df = df.drop(columns=4).drop(columns=3).drop(columns=1)
+df = df.drop(0).drop(1).drop(2)
+df.rename(columns= {0: 'Race'})
+df.rename(columns= {2:'Killed by Players'}, inplace=True)
+df.rename(columns= {0: 'Monstro'}, inplace=True)
+# Filtrozinho para conseguir remover um item especifico
+# filtro = df['Monstro'] == 'A Shielded Astral Glyph'
 df.to_csv('tabelinha.csv', index=False)
 
